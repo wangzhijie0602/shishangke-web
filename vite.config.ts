a446551ee2e68f4eb4b28bd35b90e1214fd6e7a8
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2025-02-27 17:40:50 +0800

    配置代理，解决前端跨域问题

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8081'
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
